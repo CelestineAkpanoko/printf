bd9c927517b5c199a727b5303221a7ccfe4b304a
Celestine Akpanoko <celestineakpanoko@gmail.com>
2021-07-27 07:14:03 +0100

    hot fix

--- _printf.c
+++ _printf.c
@@ -2,6 +2,8 @@
 
 void cleaner(va_list ap, buffer_t *output);
 int execute_printf(const char *format, va_list ap, buffer_t *output);
+/* just for task 0 */
+unsigned int (*specifiers_handler_task0(const char *specifier))(va_list, buffer_t *);
 
 /**
  * cleaner - cleans up _printf
@@ -61,6 +63,23 @@ int execute_printf(const char *format, va_list ap, buffer_t *output)
 	cleaner(ap, output);
 	return (ret);
 }
+unsigned int (*specifier_handler_task0(const char *specifier))(va_list, buffer_t *)
+{
+	int i;
+	converter_t0 converters[] = {
+		{'c', convert_c0},
+		{'s', convert_s0},
+		{0, NULL}
+	};
+
+	for (i = 0; converters[i].func; i++)
+	{
+		if (converters[i].specifier == *specifier)
+			return (converters[i].func);
+	}
+
+	return (NULL);
+}
 
 /**
  * _printf - Outputs a formatted string.
@@ -71,8 +90,8 @@ int execute_printf(const char *format, va_list ap, buffer_t *output)
 int _printf(const char *format, ...)
 {
 	buffer_t *output;
-	va_list args;
-	int ret;
+	va_list ap;
+	int ret, i;
 	char tmp;
 	unsigned int (*f)(va_list, buffer_t *);
 
@@ -82,23 +101,34 @@ int _printf(const char *format, ...)
 	if (output == NULL)
 		return (-1);
 
-	va_start(args, format);
+	va_start(ap, format);
 
 	/* ret = execute_printf(format, args, output); */
-	f = specifiers_handler(format + i + tmp + 1);
-	if (f != NULL)
+	for (i = 0; *(format + i); i++)
 	{
-		i += tmp + 1;
-		ret += f(ap, output);
-		continue;
-	}
-	else if (*(format + i + tmp + 1) == '\0')
-	{
-		ret = -1;
-		break;
-	}
-		ret += _memcpy(output, (format + i), 1);
-		i += (len != 0) ? 1 : 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			f = specifier_handler_task0(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(ap, output);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+
+				ret += _memcpy(output, (format + i), 1);
+				/* i += (len != 0) ? 1 : 0; */
+			}
+		}
+		
+	
+
 	cleaner(ap, output);
 	return (ret);
 }
